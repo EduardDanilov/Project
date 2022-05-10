3b8f64558c657e3ce3042e0321189d2ebe310b69
Eduard_Danilov <ed.danilov94@gmail.com>
2022-05-10 13:51:56 +0300

    build

--- site/data/html/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h1>Мой сайт №2</h>";
-
-echo "<h2><p>Для перехода на сайт нажми <a href='/index.php'>ссылку</a><br>";
-define( 'WP_USE_THEMES', true );
-require __DIR__ . '/index.php';
